7fc8d5dcec377de984da02a1d074eba723d60ff4
Breno Marques <breno@brolam.com.br>
2017-03-29 10:33:14 -0300

    feat: OhaException - Organizar todas as Exceções do aplicativo

---
+++ Android/appSupervisory/src/main/java/br/com/brolam/oha/supervisory/OhaException.java
@@ -0,0 +1,47 @@
+package br.com.brolam.oha.supervisory;
+/**
+ * Organizar todas as Exceções do aplicativo
+ * @author Breno Marques
+ * @version 1.00
+ * @since Release 01
+ */
+public class OhaException extends Exception {
+
+    public OhaException(String message) {
+        super(message);
+    }
+
+    /****************************************************************************************
+     * EnergyUseLog - Exceções para o tratamento de erros referente a tarefa de processamento
+     * dos logs utilização de energia.
+     ****************************************************************************************/
+    public static class EnergyUseLogSyncFatalError extends OhaException {
+        public EnergyUseLogSyncFatalError(String message) {
+            super(message);
+        }
+    }
+
+    public static class EnergyUseLogSdCardFatalError extends OhaException {
+        public EnergyUseLogSdCardFatalError(String message) {
+            super(message);
+        }
+    }
+
+    public static class EnergyUseLogRead extends Exception {
+        public EnergyUseLogRead(String message) {
+            super(message);
+        }
+    }
+
+    public static class EnergyUseLogReadSequenceBroken extends Exception {
+        public EnergyUseLogReadSequenceBroken(Integer previous, int current) {
+            super(String.format("EnergyUseLog read sequence was broken previous(%s) current(%s)", previous, current));
+        }
+    }
+
+    public static class EnergyUseRequestTimeOut extends Exception {
+        public EnergyUseRequestTimeOut(String message) {
+            super(message);
+        }
+    }
+}
