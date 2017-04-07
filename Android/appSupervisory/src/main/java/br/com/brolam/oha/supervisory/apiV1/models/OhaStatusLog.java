fc7ad9a9a879f4800a449fe890721feb452369bd
Breno Marques <breno@brolam.com.br>
2017-04-07 15:53:00 -0300

    feat: OhaStatusLog - recuperar a duração de execução em milissegundos da placa Arduino desde o último reset.
    Adicionando esse campo na class OhaStatusLog, será possível remover a class br.com.brolam.oha.supervisory.apiV1.models.OhaSequenceLog do projeto.

--- Android/appSupervisory/src/main/java/br/com/brolam/oha/supervisory/apiV1/models/OhaStatusLog.java
+++ Android/appSupervisory/src/main/java/br/com/brolam/oha/supervisory/apiV1/models/OhaStatusLog.java
@@ -32,18 +32,53 @@ public enum OhaStatusLog {
     /** Sinalizar que o Arduino não está respondendo ao módulo WIFI ( ESP8266)*/
     OHA_REQUEST_TIMEOUT;
 
+    private static final String FLAG_COLUMNS = "\\|";
+    private static final String FLAG_BEGIN = "<";
+    private static final String FLAG_END = ">";
+
+    /**
+     * Informar a duração de execução em milissegundos da Arduino desde o último reset.
+     */
+    private long durationBoardRunning = -1;
+
+    /**
+     * Recuperar a duração de execução em milissegundos da Arduino desde o último reset ou
+     * retornar a -1 se o valor for inválido.
+     */
+    public long getDurationBoardRunning() {
+        return durationBoardRunning;
+    }
+
     /**
      * Recuperar um OhaStatusLog conforme texto com o nome do Status
-     * @param ohaStatusLogName texto com o nome do OhaStatusLog.
+     * @param ohaStatusLogContent texto com o conteúdo do OhaStatusLog.
      * @return retornar com um OhaStatusLog válido ou nulo.
      */
-    public static OhaStatusLog getOhaStatusLog(String ohaStatusLogName){
-        for(OhaStatusLog ohaStatus : OhaStatusLog.values() ){
-            if (ohaStatusLogName.contains(ohaStatus.toString())){
-                return ohaStatus;
+    public static OhaStatusLog getOhaStatusLog(String ohaStatusLogContent){
+        for(OhaStatusLog ohaStatusLog : OhaStatusLog.values() ){
+            if (ohaStatusLogContent.contains(ohaStatusLog.toString())){
+                ohaStatusLog.durationBoardRunning = getDurationBoardRunning(ohaStatusLogContent);
+                return ohaStatusLog;
             }
         }
         return null;
+    }
+
+    /**
+     * Recuperar a duração de execução em milissegundos da placa Arduino desde o último reset.
+     * @param ohaStatusLogContent informar o conteúdo do OhaStatusLog
+     * @return duração em milissegundos ou -1 se não for possível recupear a duração.
+     */
+    private static long getDurationBoardRunning(String ohaStatusLogContent) {
+        try {
+            if ( (ohaStatusLogContent.indexOf(FLAG_BEGIN) != -1) && (ohaStatusLogContent.indexOf(FLAG_END) != -1) ) {
+                String[] values = ohaStatusLogContent.replace(FLAG_BEGIN, "").replace(FLAG_END, "").split(FLAG_COLUMNS);
+                return values.length == 2 ? Long.parseLong(values[1]) : 0;
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return -1;
     }
 
     /**
@@ -54,8 +89,8 @@ public enum OhaStatusLog {
     public static OhaStatusLog getOhaStatusLog(List<String> strings) {
         //Localicar o OhaStatusLog nos 3 ultimos itens da lista.
         for (int item = 1; item <= strings.size(); item++) {
-            String ohaStatusLogName = strings.get(strings.size() - item);
-            OhaStatusLog ohaStatusLog = getOhaStatusLog(ohaStatusLogName);
+            String ohaStatusLogContent = strings.get(strings.size() - item);
+            OhaStatusLog ohaStatusLog = getOhaStatusLog(ohaStatusLogContent);
             if ((ohaStatusLog != null) || item > 3) return ohaStatusLog;
         }
         return null;
