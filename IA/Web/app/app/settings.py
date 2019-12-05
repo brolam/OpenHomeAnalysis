63bb72a41e8ad401397f20fad78073453da305d6
brolam <breno@brolam.com.br>
2019-12-05 09:59:24 -0300

    Setup djangorestframework-jwt

--- IA/Web/app/app/settings.py
+++ IA/Web/app/app/settings.py
@@ -96,6 +96,17 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
+
 ROOT_URLCONF = 'app.urls'
 
 TEMPLATES = [
--- IA/Web/app/app/urls.py
+++ IA/Web/app/app/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_jwt.views import obtain_jwt_token
 from django.views.generic import TemplateView
 from .views import UserViewSet, OhaDeviceViewSet, OhaEnergyLogViewSet, OhaEnergyLogBatchViewSet
 
@@ -11,7 +12,8 @@ router.register(r'energyLog', OhaEnergyLogViewSet)
 router.register(r'energyLogBatch', OhaEnergyLogBatchViewSet)
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='front-end/index.html')),
+    path('', TemplateView.as_view(template_name='front-end/index.html'), name='home'),
+    path('token-auth/', obtain_jwt_token),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
