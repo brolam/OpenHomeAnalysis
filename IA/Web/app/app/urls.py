06fe7a7f97f67bb18306aa3660472a4e78565b72
brolam <brenomar@gmail.com>
2019-12-28 16:45:58 +0000

    Draft api response CSV.

--- IA/Web/app/app/urls.py
+++ IA/Web/app/app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from rest_framework import routers
 from rest_framework_jwt.views import obtain_jwt_token
 from django.views.generic import TemplateView
-from .views import UserViewSet, OhaSensorViewSet, OhaEnergyLogViewSet, OhaSensorLogBatchViewSet
+from .views import UserViewSet, OhaSensorViewSet, OhaEnergyLogViewSet, OhaSensorLogBatchViewSet, OhaEnergyLogCSVviewSet
 
 router = routers.DefaultRouter()
 router.register(r'user', UserViewSet)
@@ -11,12 +11,12 @@ router.register(r'sensor', OhaSensorViewSet)
 router.register(r'sensorLogBatch', OhaSensorLogBatchViewSet)
 router.register(r'energyLog', OhaEnergyLogViewSet)
 
-
 urlpatterns = [
     path('', TemplateView.as_view(
         template_name='front-end/index.html'), name='home'),
     path('token-auth/', obtain_jwt_token, name='token-auth'),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api/energyLogCsv/<uuid:oha_sensor_id>', OhaEnergyLogCSVviewSet.as_view(), name="energyLogCsv"),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- IA/Web/app/app/views.py
+++ IA/Web/app/app/views.py
@@ -1,34 +1,37 @@
 from django.contrib.auth.models import User
 from .models import OhaSensor, OhaEnergyLog, OhaEnergyLog, OhaSensorLogBatch
-from rest_framework import viewsets, serializers, status, permissions, authentication
+from rest_framework import viewsets, serializers, status, permissions
 from rest_framework.response import Response
+from django.http import HttpResponse
+from django.views import View
 from .serializers import UserSerializer, OhaSensorSerializer, OhaEnergyLogSerializer, OhaSensorLogBatchSerializer
+import csv
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all().order_by('-date_joined')
+    queryset = User.objects
     serializer_class = UserSerializer
+    def get_queryset(self):
+        user = self.request.user
+        return User.objects.filter(id=user.id)
 
 
 class OhaSensorViewSet(viewsets.ModelViewSet):
-    queryset = OhaSensor.objects.all()
+    queryset = OhaSensor.objects
     serializer_class = OhaSensorSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return OhaSensor.objects.filter(owner=user)
 
 
 class OhaSensorLogBatchViewSet(viewsets.ModelViewSet):
     class SensorPermissions(permissions.BasePermission):
         def has_permission(self, request, view):
             return True
-
-    class SensorAuthentication(authentication.BaseAuthentication):
-        def authenticate(self, request):
-            user = User.objects.get(username="root")
-            return (user, None)
-
     queryset = OhaSensorLogBatch.objects
     serializer_class = OhaSensorLogBatchSerializer
     permission_classes = (SensorPermissions,)
-    authentication_classes = [SensorAuthentication]
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -38,5 +41,26 @@ class OhaSensorLogBatchViewSet(viewsets.ModelViewSet):
 
 
 class OhaEnergyLogViewSet(viewsets.ModelViewSet):
-    queryset = OhaEnergyLog.objects.all().order_by('-id')[1:10]
+    queryset = OhaEnergyLog.objects
     serializer_class = OhaEnergyLogSerializer
+    def get_queryset(self):
+        user = self.request.user
+        return OhaEnergyLog.objects.filter(oha_sensor__owner=user).order_by('-id')[:10]
+
+
+class OhaEnergyLogCSVviewSet(View):
+   
+    def get(self, request, oha_sensor_id):
+        print( request.user )
+        #print( request.GET['oha_sensor_id'] )
+        # The only line to customize
+        model_class = OhaEnergyLog
+        meta = model_class._meta
+        field_names = ['id','unix_time','duration','voltage','watts1','watts2','watts3','watts_total','sensor_convection']
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+        writer.writerow(field_names)
+        for obj in model_class.objects.all():
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+        return response
